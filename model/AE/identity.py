c94787c30c1946fa9173d27b0a02964313128ec7
MiaoDragon <innocentmyl@gmail.com>
2019-10-14 22:53:00 -0700

    just return the input, not using Identity because it is not valid in previous Pytorch version

--- model/AE/identity.py
+++ model/AE/identity.py
@@ -7,7 +7,5 @@ from torch.autograd import Variable
 class Encoder(nn.Module):
     def __init__(self, input_size, output_size):
         super(Encoder, self).__init__()
-        self.encoder = nn.Identity()
     def forward(self, x):
-        out = self.encoder(x)
-        return out
+        return x
