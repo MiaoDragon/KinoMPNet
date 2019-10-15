d197b22e45eaf4ec64b383b30eae64bb72fd289c
MiaoDragon <innocentmyl@gmail.com>
2019-10-14 22:51:39 -0700

    inheritance issue

--- model/AE/identity.py
+++ model/AE/identity.py
@@ -6,7 +6,7 @@ from torch.autograd import Variable
 
 class Encoder(nn.Module):
     def __init__(self, input_size, output_size):
-        super(MLP, self).__init__()
+        super(Encoder, self).__init__()
         self.encoder = nn.Identity()
     def forward(self, x):
         out = self.encoder(x)
