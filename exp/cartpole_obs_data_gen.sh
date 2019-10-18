7c90beb0913a35010332272f2bd752c84b921ba5
MiaoDragon <innocentmyl@gmail.com>
2019-10-18 15:37:35 -0700

    finally made sure data generation code for cartpole_obs is working fine

--- exp/cartpole_obs_data_gen.sh
+++ exp/cartpole_obs_data_gen.sh
@@ -1,5 +1,5 @@
 cd ..
-python data_generation.py --env_name cartpole_obs --N 2 --NP 2 \
+python data_generation.py --env_name cartpole_obs --N 100 --NP 5000 \
 --max_iter 100000 --path_folder ./data/cartpole_obs/ --path_file path.pkl \
 --obs_file ./data/cartpole_obs/obs.pkl --obc_file ./data/cartpole_obs/obc.pkl
 cd exp
