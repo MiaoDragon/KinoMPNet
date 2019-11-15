5643429e39714fab12a2be0e784da344849fefee
MiaoDragon <innocentmyl@gmail.com>
2019-11-14 22:11:42 -0800

    start generating data

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,8 @@
 """
 using SST* to generate near-optimal paths in specified environment
 """
+import sys
+sys.path.append('deps/sparse_rrt')
 import argparse
 from sparse_rrt import _sst_module
 from sparse_rrt.systems import standard_cpp_systems
@@ -10,13 +12,26 @@ import pickle
 from sparse_rrt.systems.acrobot import Acrobot, AcrobotDistance
 from sparse_rrt.systems.point import Point
 import os
-
+import gc
 from tools.pcd_generation import rectangle_pcd
+from multiprocessing import Process, Queue
+
+
 
 def main(args):
     # set up the environment
-    if args.env_name == 'cartpole':
-        env_constr = standard_cpp_systems.CartPole
+    if args.env_name == 'pendulum':
+        env_constr = standard_cpp_systems.PSOPTPendulum
+        min_time_steps = 10
+        max_time_steps = 200
+        integration_step = 0.002
+        max_iter = 1000
+        goal_radius=0.1
+        random_seed=0
+        sst_delta_near=0.05
+        sst_delta_drain=0.02
+    elif args.env_name == 'cartpole':
+        env_constr = standard_cpp_systems.PSOPTCartPole
         obs_list = None
         obc_list = None
         goal_radius=1.5
@@ -42,6 +57,8 @@ def main(args):
         integration_step = 0.002
         near = width * 1.2
         print('generating obs...')
+
+
         for i in range(args.N):
             obs_single = []
             for j in range(args.N_obs):
@@ -76,16 +93,78 @@ def main(args):
         # convert from obs to point cloud
         obc_list = rectangle_pcd(obs_list, width, 1400)
 
-    ## TODO: add other env
-    # store the obstacles and obc first
-    file = open(args.obs_file, 'wb')
-    pickle.dump(obs_list, file)
-    file = open(args.obc_file, 'wb')
-    pickle.dump(obc_list, file)
+        ## TODO: add other env
+        # store the obstacles and obc first
+        file = open(args.obs_file, 'wb')
+        pickle.dump(obs_list, file)
+        file = open(args.obc_file, 'wb')
+        pickle.dump(obc_list, file)
 
+
+    ####################################################################################
+
+    def plan_one_path(env, start, end, out_queue, path_file, control_file, cost_file, time_file):
+        planner = _sst_module.SSTWrapper(
+            state_bounds=env.get_state_bounds(),
+            control_bounds=env.get_control_bounds(),
+            distance=env.distance_computer(),
+            start_state=start,
+            goal_state=end,
+            goal_radius=goal_radius,
+            random_seed=random_seed,
+            sst_delta_near=sst_delta_near,
+            sst_delta_drain=sst_delta_drain
+        )
+        # generate a path by using SST to plan for some maximal iterations
+        time0 = time.time()
+        #print('obs: %d, path: %d' % (i, j))
+        for iter in range(args.max_iter):
+            if iter % 500 == 0:
+                print('still alive after %d iterations' % (iter))
+            if iter % 100 == 0:
+                # from time to time use the goal
+                sample = end
+                #planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
+            else:
+                sample = np.random.uniform(low=low, high=high)
+                #planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
+            planner.step(env, min_time_steps, max_time_steps, integration_step)
+            #planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
+            solution = planner.get_solution()
+            # don't break the searching to find better solutions
+            #if solution is not None:
+            #    break
+        plan_time = time.time() - time0
+        if solution is None:
+            out_queue.put(0)
+        else:
+            print('path succeeded.')
+            path, controls, cost = solution
+            print(path)
+            path = np.array(path)
+            controls = np.array(controls)
+            cost = np.array(cost)
+
+            file = open(path_file, 'wb')
+            pickle.dump(path, file)
+            file.close()
+            file = open(control_file, 'wb')
+            pickle.dump(controls, file)
+            file.close()
+            file = open(cost_file, 'wb')
+            pickle.dump(cost, file)
+            file.close()
+            file = open(time_file, 'wb')
+            pickle.dump(plan_time, file)
+            file.close()
+            out_queue.put(1)
+    ####################################################################################
+    queue = Queue(1)
     for i in range(args.N):
         # load the obstacle by creating a new environment
-        if args.env_name == 'cartpole':
+        if args.env_name == 'pendulum':
+            env = env_constr()
+        elif args.env_name == 'cartpole':
             env = env_constr()
         elif args.env_name == 'cartpole_obs':
             env = env_constr(obs_list[i], width)
@@ -100,101 +179,56 @@ def main(args):
         paths = []
         actions = []
         costs = []
+        times = []
+        suc_n = 0
         for j in range(args.NP):
             # randomly sample collision-free start and goal
-            while True:
-                print('trial')
-                start = np.random.uniform(low=low, high=high)
-                end = np.random.uniform(low=low, high=high)
-                # set the velocity terms to zero
-                if args.env_name == 'cartpole':
-                    start[1] = 0.
-                    start[3] = 0.
-                    end[1] = 0.
-                    end[3] = 0.
-                elif args.env_name == 'cartpole_obs':
-                    start[1] = 0.
-                    start[3] = 0.
-                    end[1] = 0.
-                    end[3] = 0.
-                planner = _sst_module.SSTWrapper(
-                    state_bounds=env.get_state_bounds(),
-                    control_bounds=env.get_control_bounds(),
-                    distance=env.distance_computer(),
-                    start_state=start,
-                    goal_state=end,
-                    goal_radius=goal_radius,
-                    random_seed=random_seed,
-                    sst_delta_near=sst_delta_near,
-                    sst_delta_drain=sst_delta_drain
-                )
-                # generate a path by using SST to plan for some maximal iterations
-                time0 = time.time()
-                print('obs: %d, path: %d' % (i, j))
-                for iter in range(args.max_iter):
-                    #if iter % 100 == 0:
-                    #    # from time to time use the goal
-                    #    sample = end
-                    #    planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
-                    #else:
-                        #sample = np.random.uniform(low=low, high=high)
-                        #planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
-                    planner.step(env, min_time_steps, max_time_steps, integration_step)
-                    #planner.step_with_sample(env, sample, min_time_steps, max_time_steps, integration_step)
-                    #solution = planner.get_solution()
-                    # don't break the searching to find better solutions
-                    #if solution is not None:
-                    #    break
-                print('spent time: %f' % (time.time() - time0))
-                solution = planner.get_solution()
-                if solution is None:
-                    continue
-                else:
-                    print('path %d: succeeded.' % (j))
-                    path, controls, cost = solution
-                    print(path)
-                    path = np.array(path)
-                    paths.append(path)
-                    controls = np.array(controls)
-                    actions.append(controls)
-                    cost = np.array(cost)
-                    costs.append(cost)
-                    break
-        # store the paths to file
-        # check if obstacle exists, if not, then directly store at path_folder
-        if obs_list is None:
-            file = open(args.path_folder+args.path_file, 'wb')
-            pickle.dump(paths, file)
-            file = open(args.path_folder+args.action_file, 'wb')
-            pickle.dump(actions, file)
-            file = open(args.path_folder+args.cost_file, 'wb')
-            pickle.dump(costs, file)
-
-        else:
-            # create a new directory under path_folder
+            start = np.random.uniform(low=low, high=high)
+            end = np.random.uniform(low=low, high=high)
+            # set the velocity terms to zero
+            if args.env_name == 'cartpole':
+                start[1] = 0.
+                start[3] = 0.
+                end[1] = 0.
+                end[3] = 0.
+            elif args.env_name == 'cartpole_obs':
+                start[1] = 0.
+                start[3] = 0.
+                end[1] = 0.
+                end[3] = 0.
             dir = args.path_folder+str(i)+'/'
             if not os.path.exists(dir):
                 os.makedirs(dir)
-            file = open(dir+args.path_file, 'wb')
-            pickle.dump(paths, file)
-            file = open(dir+args.action_file, 'wb')
-            pickle.dump(actions, file)
-            file = open(dir+args.cost_file, 'wb')
-            pickle.dump(costs, file)
+            path_file = dir+args.path_file+'_%d'%(j) + ".pkl"
+            control_file = dir+args.control_file+'_%d'%(j) + ".pkl"
+            cost_file = dir+args.cost_file+'_%d'%(j) + ".pkl"
+            time_file = dir+args.time_file+'_%d'%(j) + ".pkl"
+            p = Process(target=plan_one_path, args=(env, start, end, queue, path_file, control_file, cost_file, time_file))
+            p.start()
+            p.join()
+            res = queue.get()
+            print('obtained result:')
+            print(res)
+            if res:
+                suc_n += 1
+            print('current success rate: %f' % (float(suc_n) / (j+1)))
 
+
+        print('success rate: %f' % (float(suc_n) / args.NP))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--env_name', type=str, default='cartpole')
-    parser.add_argument('--N', type=int, default=100)
+    parser.add_argument('--N', type=int, default=1)
     parser.add_argument('--N_obs', type=int, default=6)
     parser.add_argument('--NP', type=int, default=5000)
-    parser.add_argument('--max_iter', type=int, default=100000)
+    parser.add_argument('--max_iter', type=int, default=10000)
     parser.add_argument('--path_folder', type=str, default='./data/cartpole/')
-    parser.add_argument('--path_file', type=str, default='path.pkl')
-    parser.add_argument('--action_file', type=str, default='action.pkl')
-    parser.add_argument('--cost_file', type=str, default='cost.pkl')
+    parser.add_argument('--path_file', type=str, default='path')
+    parser.add_argument('--control_file', type=str, default='control')
+    parser.add_argument('--cost_file', type=str, default='cost')
+    parser.add_argument('--time_file', type=str, default='time')
     parser.add_argument('--obs_file', type=str, default='./data/cartpole/obs.pkl')
     parser.add_argument('--obc_file', type=str, default='./data/cartpole/obc.pkl')
     args = parser.parse_args()
