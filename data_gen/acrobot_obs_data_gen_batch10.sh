2e8d02c5759277194b1bb5ce4206b2c386a0ebc1
MiaoDragon <innocentmyl@gmail.com>
2020-01-14 18:15:25 -0800

    chmod

--- data_gen/acrobot_obs_data_gen_batch10.sh
+++ data_gen/acrobot_obs_data_gen_batch10.sh
@@ -1,4 +1,4 @@
-python data_generation.py --env_name acrobot_obs --N 1 --N_obs 4 --NP 1000 --s 4 --sp 0 \
+python data_generation.py --env_name acrobot_obs --N 1 --N_obs 4 --NP 1000 --s 9 --sp 0 \
 --max_iter 300000 --path_folder ../data/acrobot_obs/ \
 --obs_file ../data/acrobot_obs/obs.pkl --obc_file ../data/acrobot_obs/obc.pkl
 # 100 x 2000
--- data_gen/cartpole_obs_data_gen_batch8.sh
+++ data_gen/cartpole_obs_data_gen_batch8.sh
@@ -1,4 +1,4 @@
-python data_generation.py --env_name cartpole_obs --N 1 --N_obs 7 --NP 1000 --s 2 --sp 0 \
+python data_generation.py --env_name cartpole_obs --N 1 --N_obs 7 --NP 1000 --s 7 --sp 0 \
 --max_iter 300000 --path_folder ../data/cartpole_obs/ \
 --obs_file ../data/cartpole_obs/obs.pkl --obc_file ../data/cartpole_obs/obc.pkl
 # 100 x 2000
