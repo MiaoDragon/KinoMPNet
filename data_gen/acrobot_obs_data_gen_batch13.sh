9417c752a865471f85a9a3bfb522fb0a43f6daa1
MiaoDragon <innocentmyl@gmail.com>
2020-02-04 23:07:22 -0800

    added new data gen batch

---
+++ data_gen/acrobot_obs_data_gen_batch13.sh
@@ -0,0 +1,4 @@
+python data_generation.py --env_name acrobot_obs --N 1 --N_obs 4 --NP 1000 --s 12 --sp 0 \
+--max_iter 300000 --path_folder ../data/acrobot_obs/ \
+--obs_file ../data/acrobot_obs/obs.pkl --obc_file ../data/acrobot_obs/obc.pkl
+# 100 x 2000
