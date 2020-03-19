cce1372a446ac4353f90696fc40dfc29f16e17bd
MiaoDragon <innocentmyl@gmail.com>
2020-03-18 18:58:10 -0700

    smaller iter

--- data_gen/acrobot_simple_obs_data_gen_use_sg.sh
+++ data_gen/acrobot_simple_obs_data_gen_use_sg.sh
@@ -1,4 +1,4 @@
 python data_generation_read_sg.py --env_name acrobot --N 2 --N_obs 2 --NP 10 --s 0 --sp 0 \
---max_iter 100000 --path_folder ../data/acrobot_simple/ \
+--max_iter 30000 --path_folder ../data/acrobot_simple/ \
 --obs_file ../data/acrobot_simple/obs.pkl --obc_file ../data/acrobot_simple/obc.pkl
 # 10x1000
