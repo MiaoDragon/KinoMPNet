91425a7559db1731001628e66e2c836e5ebcf0f5
MiaoDragon <innocentmyl@gmail.com>
2020-03-18 18:43:03 -0700

    added new data generation for CSE hw

---
+++ data_gen/acrobot_simple_obs_data_gen.sh
@@ -0,0 +1,4 @@
+python data_generation.py --env_name acrobot_obs --N 2 --N_obs 2 --NP 10 --s 0 --sp 0 \
+--max_iter 300000 --path_folder ../data/acrobot_simple_obs/ \
+--obs_file ../data/acrobot_simple_obs/obs.pkl --obc_file ../data/acrobot_simple_obs/obc.pkl
+# 10x1000
