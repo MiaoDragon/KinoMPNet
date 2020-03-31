08745dee71011010392764d919aaf52fd8730bff
MiaoDragon <innocentmyl@gmail.com>
2020-03-30 17:35:11 -0700

    added model testing code

---
+++ exp/acrobot_obs_7_test_lr01_SGD_dir0.sh
@@ -0,0 +1,7 @@
+cd ..
+python neural_test.py --model_path /media/arclabdl1/HD1/YLmiao/results/KMPnet_res/acrobot_obs_7_lr0.010000_SGD/ --seen_N 10 --seen_NP 10 --total_input_size 8 \
+--AE_input_size 32 --mlp_input_size 136 --output_size 4 --learning_rate 0.01 --device 1 --start_epoch 500 \
+--data_folder ./data/acrobot_obs/ --env_type acrobot_obs_7 \
+--world_size 3.141592653589793 3.141592653589793 6.0 6.0 --direction 0 --opt SGD
+cd exp
+#10x800
