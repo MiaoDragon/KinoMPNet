11989f0bf29037b278bd389266d066bac9eddfde
MiaoDragon <innocentmyl@gmail.com>
2020-05-07 21:10:25 -0700

    Update cost_acrobot_py_model_to_cpp.sh

--- c++/cost_acrobot_py_model_to_cpp.sh
+++ c++/cost_acrobot_py_model_to_cpp.sh
@@ -1,4 +1,4 @@
 python py_model_to_cpp_cost.py --model_path /media/arclabdl1/HD1/YLmiao/results/KMPnet_res/ --no_env 1 --no_motion_paths 1 --total_input_size 8 \
---AE_input_size 32 --mlp_input_size 136 --output_size 1 --learning_rate 0.01 --device 1 --num_epochs 5000 \
+--AE_input_size 32 --mlp_input_size 136 --output_size 1 --learning_rate 0.01 --device 1 \
 --path_folder ./data/acrobot_obs/ --path_file path.pkl --start_epoch 1000 --env_type acrobot_obs_8 \
 --world_size 3.141592653589793 3.141592653589793 6.0 6.0 --direction 0 --opt SGD
