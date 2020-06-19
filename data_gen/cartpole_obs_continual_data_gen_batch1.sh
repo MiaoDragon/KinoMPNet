c072aa4797a8b97d7cc367d0fc4a52ba78d3591e
MiaoDragon <innocentmyl@gmail.com>
2020-06-18 19:02:16 -0700

    added data generation for continuing from previous batches

---
+++ data_gen/cartpole_obs_continual_data_gen_batch1.sh
@@ -0,0 +1,4 @@
+python data_generation_continual.py --env_name cartpole_obs --N 1 --N_obs 7 --NP 1000 --s 0 --sp 1000 \
+--max_iter 300000 --path_folder ../data/cartpole_obs/ \
+--obs_file ../data/cartpole_obs/obs.pkl --obc_file ../data/cartpole_obs/obc.pkl
+# 100 x 2000
